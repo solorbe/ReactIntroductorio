c8f31b0578df32d807157c1edee409032428da6e
solorbe <mariasolorbe@gmail.com>
2023-03-22 19:16:01 -0300

    hasta clase 7

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch.js";
+import { TodoList } from "./TodoList.js";
+import { TodoItem } from "./TodoItem.js";
+import { CreateTodoButton } from "./CreateTodoButton.js"
 
-function App() {
+const todos = [
+  { text: 'Cortar cebolla', completed: true},
+  { text: 'Tomar el curso de intro a React', completed: false},
+  { text: 'Llorar con la llorona', completed: false},
+  { text: 'lalalalal', completed: false},
+]
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <React.Fragment>
+   <TodoCounter />
+   <TodoSearch /> 
+   <TodoList>
+   <TodoList>
+        {todos.map((item, index)=>(
+        <TodoItem key={index} {...item}/>
+        ))}
+   </TodoList>
+   </TodoList> 
+   <CreateTodoButton /> 
+  </React.Fragment>
   );
 }
 
---
+++ src/TodoItem.js
@@ -0,0 +1,34 @@
+// import React from "react";
+
+// function TodoItem(props){
+//     return(
+//         <li>
+//             <span>C</span>
+//             <p>{props.text}</p>
+//             <span>X</span>
+//         </li>
+//     );
+// }
+
+// export { TodoItem };
+
+import React from 'react';
+import './TodoItem.css';
+
+function TodoItem(props) {
+  return (
+    <li className="TodoItem">
+      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+        √
+      </span>
+      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+        {props.text}
+      </p>
+      <span className="Icon Icon-delete">
+        X
+      </span>
+    </li>
+  );
+}
+
+export { TodoItem };
