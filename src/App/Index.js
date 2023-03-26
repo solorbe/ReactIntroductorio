7ff76d55558d0eaa72d05f02c1a92db4670ee98e
solorbe <mariasolorbe@gmail.com>
2023-03-26 08:34:51 -0300

    hasta clase 15

--- src/App/Index.js
+++ src/App/Index.js
@@ -7,35 +7,94 @@ import { AppUI } from './AppUI';
 //   { text: 'Llorar con la llorona', completed: true },
 //   { text: 'LALALALAA', completed: false },
 // ];
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
+// function useLocalStorage(itemName, initialValue) {
+//   const localStorageItem = localStorage.getItem(itemName);
+//   let parsedItem;
   
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
+//   if (!localStorageItem) {
+//     localStorage.setItem(itemName, JSON.stringify(initialValue));
+//     parsedItem = initialValue;
+//   } else {
+//     parsedItem = JSON.parse(localStorageItem);
+//   }
 
-  const [item, setItem] = React.useState(parsedItem);
+//   const [item, setItem] = React.useState(parsedItem);
 
+//   const saveItem = (newItem) => {
+//     const stringifiedItem = JSON.stringify(newItem);
+//     localStorage.setItem(itemName, stringifiedItem);
+//     setItem(newItem);
+//   };
+
+//   return [
+//     item,
+//     saveItem,
+//   ];
+// }
+
+function useLocalStorage(itemName, initialValue) 
+{
+  // Creamos el estado inicial para nuestros errores y carga
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
+  
+  React.useEffect(() => {
+  // Simulamos un segundo de delay de carga 
+    setTimeout(() => {
+      // Manejamos la tarea dentro de un try/catch por si ocurre algún error
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+        
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+        setItem(parsedItem);
+      } catch(error) {
+      // En caso de un error lo guardamos en el estado
+        setError(error);
+      } finally {
+        // También podemos utilizar la última parte del try/cath (finally) para terminar la carga
+        setLoading(false);
+      }
+    }, 5000);
+  });
   const saveItem = (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
+    // Manejamos la tarea dentro de un try/catch por si ocurre algún error
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch(error) {
+      // En caso de algún error lo guardamos en el estado
+      setError(error);
+    }
   };
-
-  return [
+  // Para tener un mejor control de los datos retornados, podemos regresarlos dentro de un objeto
+  return {
     item,
     saveItem,
-  ];
+    loading,
+    error,
+  };
 }
 
 
+
 function App() {
- 
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+    // Desestructuramos los nuevos datos de nustro custom hook
+    const {
+      item: todos,
+      saveItem: saveTodos,
+      loading,
+      error,
+    } = useLocalStorage('TODOS_V1', []);
+  
+  //const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -67,25 +126,35 @@ function App() {
     saveTodos(newTodos);
   };
   
+//   return (
+//     <AppUI
+//       totalTodos={totalTodos}
+//       completedTodos={completedTodos}
+//       searchValue={searchValue}
+//       setSearchValue={setSearchValue}
+//       searchedTodos={searchedTodos}
+//       completeTodo={completeTodo}
+//       deleteTodo={deleteTodo}
+//     />
+//   );
+// }
 
+// export default App;
 
-  
-  return (
-    <AppUI
-      totalTodos={totalTodos}
-      completedTodos={completedTodos}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      searchedTodos={searchedTodos}
-      completeTodo={completeTodo}
-      deleteTodo={deleteTodo}
-    />
-  );
-}
+ return(<AppUI
+  loading={loading}
+  error={error}
+  totalTodos={totalTodos}
+  completedTodos={completedTodos}
+  searchValue={searchValue}
+  setSearchValue={setSearchValue}
+  searchedTodos={searchedTodos}
+  completeTodo={completeTodo}
+  deleteTodo={deleteTodo}/>);
 
+ }
 export default App;
-
-
+ 
 
 
 
